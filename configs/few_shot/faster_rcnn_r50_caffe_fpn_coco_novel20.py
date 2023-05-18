bbe1d74fc0e659f78f64f4ec038ba35e2e970224
Vu Tran <vuptran@gmail.com>
2023-05-18 15:48:21 -0700

    Update configs

--- configs/few_shot/faster_rcnn_r50_caffe_fpn_coco_novel20.py
+++ configs/few_shot/faster_rcnn_r50_caffe_fpn_coco_novel20.py
@@ -60,7 +60,7 @@ fold = "seed1"
 shot = "30shot"
 model_type = "FasterRCNN"
 optimizer = dict(type="SGD", lr=0.01, momentum=0.9, weight_decay=0.0001)
-lr_config = dict(policy="fixed")
+lr_config = dict(_delete_=True, policy="fixed")
 runner = dict(_delete_=True, type="IterBasedRunner", max_iters=10000)
 evaluation = dict(interval=1000, metric="bbox")
 checkpoint_config = dict(by_epoch=False, interval=1000, max_keep_ckpts=1)
--- configs/semi_few_shot/semi_supervised_faster_rcnn_r101_caffe_fpn_coco_semi_few_novel20.py
+++ configs/semi_few_shot/semi_supervised_faster_rcnn_r101_caffe_fpn_coco_semi_few_novel20.py
@@ -103,7 +103,7 @@ percent = 1
 model_type = "SoftERTeacher"
 evaluation = dict(type="SubModulesDistEvalHook", metric="bbox", interval=4000 * 4)
 optimizer = dict(type="SGD", lr=0.01, momentum=0.9, weight_decay=0.0001)
-lr_config = dict(policy="fixed")
+lr_config = dict(_delete_=True, policy="fixed")
 runner = dict(_delete_=True, type="IterBasedRunner", max_iters=48000)
 checkpoint_config = dict(by_epoch=False, interval=4000, max_keep_ckpts=1)
 auto_resume = False
