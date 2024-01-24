5b3f8699c2f8e6f5091567cef4b4af4509ba7bed
Vu Tran <vuptran@gmail.com>
2024-01-24 21:26:11 +0000

    Update semi-supervised VOC configs to use COCO-train2017 instead of COCO-20

--- configs/few_shot/semi_supervised_faster_rcnn_r50_caffe_fpn_voc0712_base15_part2.py
+++ configs/few_shot/semi_supervised_faster_rcnn_r50_caffe_fpn_voc0712_base15_part2.py
@@ -67,17 +67,12 @@ data = dict(
         ),
         unsup=dict(
             type="RepeatDataset",
-            times=3,
+            times=1,
             dataset=dict(
                 type="CocoDataset",
-                classes=(
-                    "bicycle", "bird", "boat", "bus", "car", "cat", "chair",
-                    "dining table", "dog", "motorcycle", "person", "potted plant",
-                    "sheep", "train", "tv"
-                ),
                 ann_file="data/coco/annotations/instances_train2017.json",
                 img_prefix="data/coco/train2017/",
-                filter_empty_gt=True,
+                filter_empty_gt=False,
             )
         )
     ),
